0592d2332c5605024c32456529b4e83d5b44721b
Caoimhe Chaos <caoimhechaos@protonmail.com>
2013-03-12 16:49:38 +0100

    Implement a base64 converter and put it into its own library. It's named crypto although it only contains an encoding right now, but later we'll need more libssl/libcrypto bindings there.

---
+++ crypto/base64.cc
@@ -0,0 +1,97 @@
+/*-
+ * Copyright (c) 2013 Caoimhe Chaos <caoimhechaos@protonmail.com>,
+ *                    Ancient Solutions. All rights reserved.
+ *
+ * Redistribution and use in source and binary forms, with or without
+ * modification, are permitted provided that the following conditions
+ * are met:
+ *
+ * 1. Redistributions  of source code must retain  the above copyright
+ *    notice, this list of conditions and the following disclaimer.
+ * 2. Redistributions  in   binary  form  must   reproduce  the  above
+ *    copyright  notice, this  list  of conditions  and the  following
+ *    disclaimer in the  documentation and/or other materials provided
+ *    with the distribution.
+ *
+ * THIS  SOFTWARE IS  PROVIDED BY  ANCIENT SOLUTIONS  AND CONTRIBUTORS
+ * ``AS IS'' AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT
+ * LIMITED TO,  THE IMPLIED WARRANTIES OF  MERCHANTABILITY AND FITNESS
+ * FOR A  PARTICULAR PURPOSE  ARE DISCLAIMED.  IN  NO EVENT  SHALL THE
+ * FOUNDATION  OR CONTRIBUTORS  BE  LIABLE FOR  ANY DIRECT,  INDIRECT,
+ * INCIDENTAL,   SPECIAL,    EXEMPLARY,   OR   CONSEQUENTIAL   DAMAGES
+ * (INCLUDING, BUT NOT LIMITED  TO, PROCUREMENT OF SUBSTITUTE GOODS OR
+ * SERVICES; LOSS OF USE,  DATA, OR PROFITS; OR BUSINESS INTERRUPTION)
+ * HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT,
+ * STRICT  LIABILITY,  OR  TORT  (INCLUDING NEGLIGENCE  OR  OTHERWISE)
+ * ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED
+ * OF THE POSSIBILITY OF SUCH DAMAGE.
+ */
+
+#ifdef HAVE_CONFIG_H
+#include "config.h"
+#endif /* HAVE_CONFIG_H */
+
+#include <string>
+
+#include <openssl/sha.h>
+#include <openssl/hmac.h>
+#include <openssl/evp.h>
+#include <openssl/bio.h>
+#include <openssl/buffer.h>
+
+#ifdef HAVE_STRING_H
+#include <string.h>
+#endif /* HAVE_STRING_H */
+#ifdef HAVE_MEMORY_H
+#include <memory.h>
+#endif /* HAVE_MEMORY_H */
+
+#include "base64.h"
+#include "scopedptr.h"
+
+namespace toolbox
+{
+using std::string;
+
+string
+Base64::Encode(const string& input)
+{
+	BIO *bmem, *b64;
+	BUF_MEM *bptr;
+
+	b64 = BIO_new(BIO_f_base64());
+	bmem = BIO_new(BIO_s_mem());
+	b64 = BIO_push(b64, bmem);
+	BIO_write(b64, input.data(), input.length());
+	BIO_flush(b64);
+	BIO_get_mem_ptr(b64, &bptr);
+
+	int len = bptr->length;
+	ScopedPtr<char> buff(new char[len]);
+	memset(buff.Get(), 0, len);
+	memcpy(buff.Get(), bptr->data, len-1);
+
+	BIO_free_all(b64);
+
+	return string(buff.Get(), len - 1);
+}
+
+string
+Base64::Decode(const string& input)
+{
+	BIO *b64, *bmem;
+
+	ScopedPtr<char> buffer(new char[input.length() + 1]);
+	memset(buffer.Get(), 0, input.length() + 1);
+
+	b64 = BIO_new(BIO_f_base64());
+	bmem = BIO_new_mem_buf(const_cast<char*>(input.data()),
+		       	input.length());
+	bmem = BIO_push(b64, bmem);
+
+	int len = BIO_read(bmem, buffer.Get(), input.length() + 1);
+	BIO_free_all(bmem);
+
+	return string(buffer.Get(), len);
+}
+}  // namespace toolbox
---
+++ crypto/base64_test.cc
@@ -0,0 +1,36 @@
+/*
+ * Tests for the QT Singleton implementation.
+ *
+ *  Created on: Nov 6, 2012
+ *      Author: Caoimhe Clausen
+ */
+
+#include <gtest/gtest.h>
+#include "base64.h"
+
+namespace toolbox {
+namespace testing {
+
+class Base64Test : public ::testing::Test {
+protected:
+	virtual void SetUp()
+	{
+	}
+
+	virtual void TearDown()
+	{
+	}
+};
+
+TEST_F(Base64Test, Encode)
+{
+	EXPECT_EQ("YXNkZg==", Base64::Encode("asdf"));
+}
+
+TEST_F(Base64Test, Decode)
+{
+	EXPECT_EQ("asdf", Base64::Decode("YXNkZg==\n"));
+}
+
+} /* namespace testing */
+} /* namespace toolbox */
